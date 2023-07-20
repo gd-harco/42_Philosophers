0ebb3be450c46e07c3d5c1bdfce2448f6b61d688
gd-harco <gd-harco@student.42lyon.fr>
2023-07-20 16:06:02 +0200

    progr

--- includes/philo.h
+++ includes/philo.h
@@ -1,31 +1,10 @@
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <stdio.h>
-# include <stdlib.h>
 # include <pthread.h>
-# include <strings.h>
+# include <sys/time.h>
+# include <malloc.h>
+# include <printf.h>
 # include <unistd.h>
-# include <stdbool.h>
-# include <strings.h>
-# define psleep(sec) sleep ((sec))
-# define INITIAL_STOCK   20
-# define NB_CLIENTS      5
-
-/* Structure stockant les informations des threads clients et du magasin. */
-typedef struct s_store
-{
-	int stock;
-
-	pthread_t		thread_store;
-	pthread_t		thread_clients [NB_CLIENTS];
-	pthread_mutex_t	mutex_stock;
-}	t_store;
-
-static t_store store =
-{
-	.stock = INITIAL_STOCK,
-	.mutex_stock = PTHREAD_MUTEX_INITIALIZER,
-};
-
+# include <stdlib.h>
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,84 +1,28 @@
 # include "philo.h"
 
-static int	get_random(int max);
-static void	*store_function(void *p_data);
-static void	*client_function(void *p_data);
+
+inline	ssize_t	get_time(ssize_t startup_time)
+{
+	struct timeval	time;
+	ssize_t			res;
+
+	gettimeofday(&time, NULL);
+	res = time.tv_sec;
+	res *= 1
+}
 
 int main(void)
 {
- 	int i = -1;
-	int	ret = 0;
+	struct timeval	time;
+	int				i;
 
-	printf("Creation du thread store\n");
-	ret = pthread_create(&store.thread_store, NULL, store_function, NULL);
-	if (!ret)
+	while (i < 5)
 	{
-		printf("Creation des thread client\n");
-		while (++i < NB_CLIENTS)
-		{
-			ret = pthread_create(&store.thread_clients[i], NULL,
-				client_function, (void *)i);
-			if (!ret)
-				printf("thread numero %d cree avec succes\n", i);
-			else
-		dprintf(STDERR_FILENO, "%s", strerror(ret));
-		}
+		gettimeofday(&time, NULL);
+		printf("current time in sec: %zu\n", time.tv_sec);
+		printf("current time in usec: %zu\n", time.tv_usec);
+		sleep(5);
+		i++;
 	}
-	else
-		dprintf(STDERR_FILENO, "%s", strerror(ret));
-	i = 0;
-	while (i < NB_CLIENTS)
-		pthread_join(store.thread_clients[i++], NULL);
-	pthread_join(store.thread_store, NULL);
-}
-
-static int	get_random(int max)
-{
-	double	val;
-
-	val = (double)max * rand();
-	val = val / (RAND_MAX + 1.0);
-	return ((int)val);
-}
-
-static void	*store_function(void *p_data)
-{
-	while (true)
-	{
-		printf("thread store demande a lock le mutex\n");
-		pthread_mutex_lock(&store.mutex_stock);
-		printf("Thread store viens de lock le mutex\n");
-		if (store.stock <= 0)
-		{
-			store.stock = INITIAL_STOCK;
-			printf("The store is full again, restocked to %d\n", store.stock);
-		}
-		printf("Thread store va unlock le mutex\n");
-		pthread_mutex_unlock(&store.mutex_stock);
-	}
-	return (NULL);
-}
-
-static void * client_function (void * p_data)
-{
-	int nb = (int) p_data;
-
-	while (true)
-	{
-		int val = get_random (6);
-
-		printf("thread %d demande a lock le mutex\n", nb);
-		pthread_mutex_lock(&store.mutex_stock);
-		printf("thread %d viens de lock le mutex\n", nb);
-		psleep (get_random (3));
-
-		store.stock = store.stock - val;
-		printf (
-			"Client %d prend %d du stock, reste %d en stock !\n",
-			nb, val, store.stock
-		);
-		printf("thread %d libere le mutex\n", nb);
-		pthread_mutex_unlock(&store.mutex_stock);
-	}
-	return NULL;
+	return (EXIT_SUCCESS);
 }

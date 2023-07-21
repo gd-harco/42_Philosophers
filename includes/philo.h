2bb8661285a017ff91729dc993aed3925c41182b
gd-harco <gd-harco@student.42lyon.fr>
2023-07-21 13:38:32 +0200

    created struct fork

--- includes/philo.h
+++ includes/philo.h
@@ -1,6 +1,7 @@
 #ifndef PHILO_H
 # define PHILO_H
 
+//########### SYSTEM LIB HEADERS ##########//
 # include <pthread.h>
 # include <sys/time.h>
 # include <malloc.h>
@@ -8,5 +9,17 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdbool.h>
+
+//----------- Struct -----------//
+
+typedef struct s_fork	t_fork;
+
+struct s_fork
+{
+	t_fork			*previous;
+	bool			available;
+	pthread_mutex_t	f_mutex;
+	t_fork			*next;
+};
 
 #endif

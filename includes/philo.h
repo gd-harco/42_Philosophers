6ceff8e6749470b67585c5b1de74fc39c649fbb2
gd-harco <gd-harco@student.42lyon.fr>
2023-07-21 13:20:27 +0200

    created function get_elapsed time

--- includes/philo.h
+++ includes/philo.h
@@ -7,4 +7,6 @@
 # include <printf.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdbool.h>
+
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,28 +1,35 @@
 # include "philo.h"
 
+static int	get_elapsed_time(struct timeval *initial_time);
 
-inline	ssize_t	get_time(ssize_t startup_time)
-{
-	struct timeval	time;
-	ssize_t			res;
-
-	gettimeofday(&time, NULL);
-	res = time.tv_sec;
-	res *= 1
-}
 
 int main(void)
 {
-	struct timeval	time;
+	struct timeval	initial_time;
+	int				elapsed_time;
 	int				i;
 
-	while (i < 5)
+	gettimeofday(&initial_time, NULL);
+	while (true)
 	{
-		gettimeofday(&time, NULL);
-		printf("current time in sec: %zu\n", time.tv_sec);
-		printf("current time in usec: %zu\n", time.tv_usec);
-		sleep(5);
-		i++;
+		elapsed_time = get_elapsed_time(&initial_time);
+		printf("Time elapsed since program start: %d\n", elapsed_time);
+		usleep(1000000);
 	}
 	return (EXIT_SUCCESS);
+}
+
+/**
+ * @brief returns the elapsed time in milliseconds
+ * @param initial_time a pointer to a timeval struct r
+ * epresenting the initial time, * set at the beginning of the program
+ * @return an int representing the elapsed time in milliseconds
+ */
+inline int	get_elapsed_time(struct timeval *initial_time)
+{
+	struct timeval	current_time;
+
+	gettimeofday(&current_time, NULL);
+	return ((current_time.tv_sec - initial_time->tv_sec) * 1000
+		+ (current_time.tv_usec - initial_time->tv_usec) / 1000);
 }

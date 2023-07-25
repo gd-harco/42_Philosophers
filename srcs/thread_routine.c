d65dcb4e86cc00b6b8fd076cc6ed7a839fc480c9
gd-harco <gd-harco@student.42lyon.fr>
2023-07-25 12:31:38 +0200

    fixed log tc printing the print time instead of the action time

--- srcs/thread_routine.c
+++ srcs/thread_routine.c
@@ -2,6 +2,7 @@
 
 static void	philo_sleep(t_philo *philo);
 static void	philo_eat(t_philo *philo, int *time_last_eat);
+static void	philo_think(t_philo *philo);
 static void	take_fork(t_philo *philo, t_fork *fork);
 static void	rest_fork(t_fork *fork);
 
@@ -11,21 +12,16 @@ void	*thread_routine(void *thread)
 	int		time_last_eat;
 
 	philo = (t_philo *)thread;
+	if (philo->philo_id % 2 == 1)
+		philo_think(philo);
+	else
+		philo_eat(philo, &time_last_eat);
 	while (philo->is_alive)
 	{
 		if (philo->right_fork->available && philo->left_fork->available)
 			philo_eat(philo, &time_last_eat);
 		else
-		{
-			pthread_mutex_lock(&philo->mutex_list->print_mutex);
-			printf("%d %d is thinking\n", get_elapsed_time(philo->initial_time),
-				philo->philo_id);
-			pthread_mutex_unlock(&philo->mutex_list->print_mutex);
-			while (!philo->right_fork->available
-				&& !philo->left_fork->available)
-				usleep(1000);
-			continue ;
-		}
+			philo_think(philo);
 		philo_sleep(philo);
 	}
 	return (NULL);
@@ -35,12 +31,14 @@ static void	philo_eat(t_philo *philo, int *time_last_eat)
 {
 	int	start_time;
 	int	cur_time;
+	int	action_time;
 
 	take_fork(philo, philo->left_fork);
 	take_fork(philo, philo->right_fork);
+	action_time = get_elapsed_time(philo->initial_time);
 	start_time = get_initial_time();
 	pthread_mutex_lock(&philo->mutex_list->print_mutex);
-	printf("%d %d is eating\n", start_time, philo->philo_id);
+	printf("%d %d is eating\n", action_time, philo->philo_id);
 	pthread_mutex_unlock(&philo->mutex_list->print_mutex);
 	cur_time = get_elapsed_time(start_time);
 	while (cur_time < philo->time_left_eat)
@@ -54,23 +52,40 @@ static void	philo_sleep(t_philo *philo)
 {
 	int	start_time;
 	int	cur_time;
+	int	action_time;
 
 	start_time = get_initial_time();
+	action_time = get_elapsed_time(philo->initial_time);
 	pthread_mutex_lock(&philo->mutex_list->print_mutex);
-	printf("%d %d is sleeping\n", start_time, philo->philo_id);
+	printf("%d %d is sleeping\n", action_time, philo->philo_id);
 	pthread_mutex_unlock(&philo->mutex_list->print_mutex);
 	cur_time = get_elapsed_time(start_time);
 	while (cur_time < philo->time_left_sleep)
 		cur_time = get_elapsed_time(start_time);
 }
 
+static void	philo_think(t_philo *philo)
+{
+	int	action_time;
+
+	action_time = get_elapsed_time(philo->initial_time);
+	pthread_mutex_lock(&philo->mutex_list->print_mutex);
+	printf("%d %d is thinking\n", action_time, philo->philo_id);
+	pthread_mutex_unlock(&philo->mutex_list->print_mutex);
+	while (!philo->right_fork->available
+		&& !philo->left_fork->available)
+		usleep(1000);
+}
+
 static void	take_fork(t_philo *philo, t_fork *fork)
 {
+	int	action_time;
+
 	pthread_mutex_lock(&fork->f_mutex);
 	fork->available = false;
+	action_time = get_elapsed_time(philo->initial_time);
 	pthread_mutex_lock(&philo->mutex_list->print_mutex);
-	printf("%d %d has taken a fork\n", get_elapsed_time(philo->initial_time),
-		philo->philo_id);
+	printf("%d %d has taken a fork\n", action_time, philo->philo_id);
 	pthread_mutex_unlock(&philo->mutex_list->print_mutex);
 }
 
